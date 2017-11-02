a259af41ea4bf90ff6b1444390a190e6ae5cc402
nicolepintea <nicole.pintea@gmail.com>
2017-11-02 16:39:27 +0100

    4 done

--- ShellProj_Datastructures_Memory/ShellProj_Datastructures_Memory/Program.cs
+++ ShellProj_Datastructures_Memory/ShellProj_Datastructures_Memory/Program.cs
@@ -163,7 +163,7 @@ namespace ShellProj_Datastructures_Memory
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
-            Stack<string> theList = new Stack<string>();
+            /*Stack<string> ReverseText = new Stack<string>();
             bool loopCase = true;
             while (loopCase)
             {
@@ -174,12 +174,12 @@ namespace ShellProj_Datastructures_Memory
                 {
 
                     case '+':
-                        theList.Push(value);
-                        Console.WriteLine("The next value to pop is: " + theList.Peek());
+                        ReverseText.Push(value);
+                        Console.WriteLine("The next value to pop is: " + ReverseText.Peek());
                         break;
                     case '-':
-                        theList.Pop();
-                        Console.WriteLine("The next value to pop is: " + theList.Peek());
+                        ReverseText.Pop();
+                        Console.WriteLine("The next value to pop is: " + ReverseText.Peek());
                         break;
                     case '0':
                         loopCase = false;
@@ -189,6 +189,74 @@ namespace ShellProj_Datastructures_Memory
                         break;
                 }
             }
+        }*/
+            Stack<string> testStack = new Stack<string>();
+            do
+            {
+                Console.WriteLine("\n Press 1 to Push," +
+             "\n Press 2 to Pop,\n Press 3 to Reverse a String\n press 0 to quit ");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()[0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+
+                if (input == '0')
+                {
+                    break;
+                }
+
+                switch (input)
+                {
+                    case '1':
+                        Console.WriteLine("Please provide input:");
+                        string name = Console.ReadLine();
+                        testStack.Push(name);
+                        foreach (var item in testStack)
+                        {
+                            Console.WriteLine("Names in the stack are " + item);
+                        }
+                        Console.WriteLine("Count in the stack: " + testStack.Count);
+                        break;
+                    case '2':
+                        if (testStack.Count > 0)
+                        {
+                            Console.WriteLine("Implementing Last In First Out");
+                            Console.WriteLine("Name removed from the stack is " + testStack.Pop());
+                            foreach (var item in testStack)
+                            {
+                                Console.WriteLine("Names in the stack are " + item);
+                            }
+                            Console.WriteLine("Count in the stack :" + testStack.Count);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Stack is empty,name cannot be deleted");
+                        }
+                        break;
+                    case '3':
+                        Console.WriteLine("Enter some input value");
+                        string inputString = Console.ReadLine();
+                        Stack<string> inputStack = new Stack<string>();
+                        for (int i = 0; i < inputString.Length; i++)
+                            inputStack.Push(inputString.Substring(i, 1));
+                        string resultstring = string.Empty;
+                        for (int i = 0; i < inputString.Length; i++)
+                            resultstring += inputStack.Pop();
+                        Console.WriteLine("Reversed string is " + resultstring);
+                        break;
+
+
+                }
+
+
+            } while (true);
+            Console.Clear();
         }
 
         static void CheckParanthesis()
@@ -198,12 +266,87 @@ namespace ShellProj_Datastructures_Memory
              * Example of correct: (()), {}, [({})]
              * Example of incorrect: (()]), [), {[()}]
              */
-            
+            Console.WriteLine("Please enter some input");
+            string input = Console.ReadLine();
+
+            Dictionary<char, char> bracketPairs = new Dictionary<char, char>() {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' },
+            { '<', '>' }
+                      };
+
+            Stack<char> brackets = new Stack<char>();
+            //string result;
+            try
+            {
+                // Iterate through each character in the input string
+                foreach (char c in input)
+                {
+                    // check if the character is one of the 'opening' brackets
+                    if (bracketPairs.Keys.Contains(c))
+                    {
+                        // if yes, push to stack
+                        brackets.Push(c);
+                    }
+                    else {
+                        // check if the character is one of the 'closing' brackets
+                        if (bracketPairs.Values.Contains(c))
+                        {
+                            // check if the closing bracket matches the 'latest' 'opening' bracket
+                            if (c == bracketPairs[brackets.First()])
+                            {
+                                brackets.Pop();
+                            }
+                            else
+                            {
+                                brackets.Push('c');
+                                // if not, its an unbalanced string
+                                //  Console.WriteLine("Its an unbalanced string");
+                            }
+                        }
+                        else
+                            // continue looking
+                            continue;
+                    }
+
+                }
+
+            }
+            catch
+            {
+                // an exception will be caught in case a closing bracket is found, 
+                // before any opening bracket.
+                // that implies, the string is not balanced.
+
+                //  Console.WriteLine("Closing bracket is found first");
+                brackets.Push('c');
 
 
+            }
 
+            // Ensure all brackets are closed
 
+            if (brackets.Count() == 0)
+            {
+
+                Console.WriteLine("String is Balanced");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("String is not Balanced");
+                Console.ReadLine();
+
+            }
+            Console.Clear();
         }
 
+
+
+
     }
+
+
 }
